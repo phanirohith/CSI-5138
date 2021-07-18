0f5f1760cd9f921c0089077ca933e882eab3a234
phanirohith <rohith.phani7@gmail.com>
2021-07-17 20:32:53 -0400

    updated version of jhava

--- Assignment 1/HelloWorld.java
+++ Assignment 1/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
 	System.out.println ("Hello World!");
+	System.out.println ("Hi Rohith!");
     }
 }
 {"mode":"full","isActive":false}
