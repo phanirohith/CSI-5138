18925bcc9edd6503362d540ac6fd561aeb4c0b8d
phanirohith <rohith.phani7@gmail.com>
2021-07-17 20:36:04 -0400

    just main

---
+++ Assignment 1/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+	System.out.println ("Hello World!");
+	System.out.println ("Hi Rohith!");
+    }
+}
+{"mode":"full","isActive":false}
